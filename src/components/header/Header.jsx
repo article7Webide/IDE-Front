7ba1b30f31f75ba0d9782b8b4df3883a84ee08f0
eunv <dmsql1091@naver.com>
2023-12-14 01:35:33 +0900

    css 변경

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,18 +4,22 @@ import { IoMdMenu } from "react-icons/io";
 import { FiLogOut } from "react-icons/fi";
 import { FaMessage } from "react-icons/fa6";
 import { FaUserCircle } from "react-icons/fa";
+import { LiaToggleOnSolid,LiaToggleOffSolid  } from "react-icons/lia";;
 
 const Header = () => {
 
+  const [ isDarkMode, setIsDarkMode ] = useState(false);
   const [menuView, setMenuView] = useState(false);
 
   return (
-    <div className={styles.header}>
-      <div className={styles.header_wrapper}>
+    <div className={styles.header} >
+      <div className={styles.header_wrapper} style={{}}>
         <div className={styles.left_side}>
           <button className={styles.icons} onClick={()=>{setMenuView(!menuView)}} > {/* 햄버거바 클릭시 view 상태 반대로 */}
             <IoMdMenu size={24} border />
-            
+          </button>
+          <button className={styles.icons}>
+            <LiaToggleOnSolid size={24} />
           </button>
         </div>
         <div className={styles.right_side}>
