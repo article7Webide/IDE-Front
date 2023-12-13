d3ffc9de3fc66a55f192fbcdce7169a45e26bc9d
eunv <dmsql1091@naver.com>
2023-12-13 16:50:28 +0900

    file sidebar update

---
+++ src/components/file-sidebar/FileSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './FileSidebar.module.scss'
+
+
+const FileSidebar = () => {
+  return (
+    <div>FileSidebar</div>
+  )
+}
+
+export default FileSidebar
