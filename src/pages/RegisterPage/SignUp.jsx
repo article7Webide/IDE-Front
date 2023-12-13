d32834f10b6ca8ac96d26941ac2ed3ddc8b9526f
eunv <dmsql1091@naver.com>
2023-12-13 16:51:10 +0900

    signup 수정

--- src/pages/RegisterPage/SignUp.jsx
+++ src/pages/RegisterPage/SignUp.jsx
@@ -1,138 +1,94 @@
 import React, { useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
 import styles from './SignUp.module.scss';
 import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   watch,
-  //   formState: { errors },
-  //   setFocus,
-  // } = useForm();
-
+  
   const navigate = useNavigate();
 
-  const [userId, setUserId] = useState('');
-  const [userPw, setUserPw] = useState('');
-  const [checkPw, setCheckPw] = useState('');
-  const [nickName, setNickName] = useState('');
-  const [userData, setUserData] = useState([]);
 
-  const onUserIdHandler = (event) => {
-    setUserId(event.currentTarget.value);
-  }
-  const onNameHandler = (event) => {
-    setNickName(event.currentTarget.value);
-  }
-  const onPasswordHandler = (event) => {
-    setUserPw(event.currentTarget.value);
-  }
-  const onConfirmPasswordHandler = (event) => {
-    setCheckPw(event.currentTarget.value);
-  }
-  const onSignup = (e) => {
-    e.preventDefault();
-    if (userPw !== checkPw) {
-      return alert("비밀번호와 비밀번호 확인이 같아야 합니다.");
-    }
+  const [registerValues, setRegisterValues] = useState({
+    userId: "",
+    password: "",
+    checkpw: "",
+    nickname: "",
+  });
 
-    setUserData((prevUserData) => {
-      return [
-        ...prevUserData,
-        {
-          name: { nickName },
-          email: { userId },
-          pw: { userPw },
-          pwCk: { checkPw },
-        },
-      ];
-    });
-    console.log({ userData });
-
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setRegisterValues((prevValues) => ({ ...prevValues, [name]: value }));
   };
-  // const onSubmitHandler = (event) => {
-  //     event.preventDefault();
 
-  //     if(Password !== ConfirmPassword){
-  //         return alert('비밀번호와 비밀번호 확인이 같지 않습니다.')
-  //     }
-
-  //     let body = {
-  //         email: Email,
-  //         name: Name,
-  //         password: Password,
-  //         confirmPassword: ConfirmPassword,
-  //     }
-
-  // dispatch(registerUser(body))
-  // .then(response => {
-  //     if(response.payload.success){
-  //         props.history.push('/loginPage')
-  //     } else {
-  //         alert('Error')
-  //     }
-  // })
-  // }
-  /* 비밀번호 일치를 위한 useRef */
-  // const passwordInputRef = useRef(null);
-  // passwordInputRef.current = watch('password');
+  const checkSignup = async () => {
+    //비밀번호 일치 확인
+    if (registerValues.password !== registerValues.checkpw) {
+      alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+      return;
+    }
+    //모든 필드가 채워져 있는지 확인
+    if (!registerValues.userId || !registerValues.password || !registerValues.name) {
+      alert("모든 사항을 입력해주세요");
+      return;
+    }
+    //api 연결
+    try {
+      const response = await axios.post(
+        "http://api/~~signup",
+        {
+          userId: registerValues.userId,
+          password: registerValues.password,
+          nickname: registerValues.nickname,
+        }
+      );
+      if (response.data && response.data.message) {
+        alert(response.data.message);
+        navigate("/");
+      } else {
+        alert("회원가입에 실패하였습니다.");
+      }
+    } catch (error) {
+      console.error("Signup error!", error);
+      alert("회원가입 중 오류가 발생하였습니다.");
+    }
+    // 서버 요청 추가 할 사항 있으면 할 것
+  };
 
 
   return (
     <div className={styles.signup}>
       <div className={styles.container}>
         <h1>Sign Up</h1>
-        <form onSubmit={onSignup}>
+        <form onSubmit={checkSignup}>
           <input
+            name='userId'
             placeholder='아이디'
             type='text'
-            value={userId}
-            onChange={onUserIdHandler}
+            onChange={handleInputChange}
           />
           <input
             placeholder='비밀번호'
             type='password'
             name='password'
-            value={userPw}
-            onChange={onPasswordHandler}
-          // {...register('password', {
-          //   required: true,
-          //   pattern:
-          //     /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,15}$/i,
-          // })}
-          // {errors.password && errors.password.type === 'required' && (
-          //   <p>이 칸을 입력해주세요.</p>
-          // )}
-          // {errors.password && errors.password.type === 'pattern' && (
-          //   <p>비밀번호 형식이 올바르지 않습니다.</p>
-          // )}
+            autoComplete="off"
+            onChange={handleInputChange}
           />
+
           <input
             placeholder='비밀번호 확인'
             type='password'
-            name='confirm'
-            value={checkPw}
-            onChange={onConfirmPasswordHandler}
-          // {...register('confirm', {
-          //   required: true,
-          //   validate: (value) => value === passwordInputRef.current,
-          // })}
+            name='checkpw'
+            autoComplete='off'
+            onChange={handleInputChange}
           />
-          {/* {errors.confirm && errors.confirm.type === 'required' && (
-          <p>비밀번호를 다시 입력해주세요</p>
-        )}
-          {errors.confirm && errors.confirm.type === 'validate' && (
-          <p>비밀번호가 일치하지 않습니다.</p>
-        )} */}
           <input
             placeholder='닉네임'
             type='text'
-            value={nickName}
-            onChange={onNameHandler}
+            name='nickname'
+            onChange={handleInputChange}
           />
-          <button type="submit">회원가입</button>
+          <button type="submit" className='registerbtn'>회원가입</button>
           <div className={styles.already_member}>
             이미 계정이 있습니까?
             <span onClick={() => navigate("/")}> 로그인</span>
