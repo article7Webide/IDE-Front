a2c0117d5183b4144bc62337ee5f2897bf3c2ab2
eunv <dmsql1091@naver.com>
2023-12-13 16:50:01 +0900

    main page 경로 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import SignIn from './pages/LoginPage/SignIn';
 import SignUp from './pages/RegisterPage/SignUp';
 import Mypage from './pages/MyPage/Mypage';
+import MainPage from './pages/MainPage/MainPage';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<SignIn/>}/>
+        <Route path="/main" element={<MainPage/>}/>
         <Route path="/register" element={<SignUp/>}/>
         <Route path="/mypage" element={<Mypage/>}/>
       </Routes>
