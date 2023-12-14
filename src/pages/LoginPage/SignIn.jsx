a90ebbabe025d449a431806dd2b7329799412153
eunv <dmsql1091@naver.com>
2023-12-14 16:45:11 +0900

    css 변경

--- src/pages/LoginPage/SignIn.jsx
+++ src/pages/LoginPage/SignIn.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import styles from './SignIn.module.scss';
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const SignIn2 = (props) => {
-  const [id,setId] = useState("")
+const SignIn = (props) => {
+  const [id, setId] = useState("")
   const [password, setPassword] = useState("")
   const [idError, setIdError] = useState("")
   const [passwordError, setPasswordError] = useState("")
@@ -14,75 +14,67 @@ const SignIn2 = (props) => {
     setIdError("")
     setPasswordError("")
 
-  if ("" === id) {
-    setIdError("아이디를 입력해주세요")
-    return
+    if ("" === id) {
+      setIdError("아이디를 입력해주세요")
+      return
+    }
+
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(id)) {
+      setIdError("정확한 아이디를 입력해주세요")
+      return
+    }
+
+    if ("" === password) {
+      setPasswordError("비밀번호를 입력해주세요")
+      return
+    }
+
+    if (password.length < 7) {
+      setPasswordError("비밀번호를 8글자이상입력해주세요")
+      return
+    }
   }
 
-  if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(id)) {
-    setIdError("정확한 아이디를 입력해주세요")
-    return
-  }
-
-  if ("" === password) {
-    setPasswordError("비밀번호를 입력해주세요")
-    return
-  }
-
-  if (password.length < 7) {
-    setPasswordError("비밀번호를 8글자이상입력해주세요")
-    return
-  }
-}
 
 
-
-return <div className={"mainContainer"}>
+  return (
+    <div className={styles.sign}>
+      <div className={styles.container}>
         <div className={"titleContainer"}>
-          <div>Login</div>
+          <h1>Login</h1>
         </div>
 
         <br />
+        <div className={styles.formSignin}>
+          <div className={"inputContainer"}>
+            <input
+              value={id}
+              placeholder="아이디를 입력해주세요."
+              onChange={ev => setId(ev.target.value)}
+              className={"inputBox"} />
+            <label className="errorLabel">{idError}</label>
+          </div>
+          <div className={"inputContainer"}>
+            <input
+              value={password}
+              placeholder="비밀번호를 입력해주세요"
+              onChange={ev => setPassword(ev.target.value)}
+              className={"inputBox"} />
+            <label className="errorlabel">{passwordError}</label>
+          </div>
 
-        <div className={"inputContainer"}>
-          <input 
-            value =  {id}
-            placeholder = "아이디를 입력해주세요."
-            onChange = {ev => setId(ev.target.value)}
-            className = {"inputBox"} />
-          <label className = "errorLabel">{idError}</label>  
+          <button type="submit" className={styles.registerbtn} onClick={onButtonClick}>로그인</button>
+          <div className={styles.already_member}>
+            아직 회원이 아닙니까?
+            <span onClick={() => navigate("/register")}> 회원가입</span>
+          </div>
         </div>
 
-        <br />
+      </div>
 
-        <div className={"inputContainer"}>
-          <input
-            value = {password}
-            placeholder = "비밀번호를 입력해주세요"
-            onChange = {ev => setPassword(ev.target.value)}
-            className = {"inputBox"} />
-          <label className = "errorlabel">{passwordError}</label>
-        </div>
 
-        <br />
-
-        <div className = {"inputContainer"}>
-          <input 
-            className = {"inputButton"}
-            type="button"
-            onClick={onButtonClick}
-            value={"로그인"}/>  
-        </div>
-        <div className = {"inputContainer"}>
-          <input 
-            className = {"inputButton"}
-            type="button"
-            onClick={onButtonClick}
-            value={"회원가입"}/>  
-        </div>
-    
-</div>
-
+    </div>
+  )
 }
 
-export default SignIn2
+export default SignIn
