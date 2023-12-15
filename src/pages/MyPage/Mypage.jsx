8875fb63e620966ab5059c4346d818de4e66afb5
eunv <dmsql1091@naver.com>
2023-12-15 16:02:44 +0900

    취소 버튼 추가, css 파일 변경

--- src/pages/MyPage/Mypage.jsx
+++ src/pages/MyPage/Mypage.jsx
@@ -1,45 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Mypage.css';
+import styles from './Mypage.module.scss'
+import { Link, useNavigate } from 'react-router-dom/dist';
 
 const MyPage = () => {
-  const [userInfo, setUserInfo] = useState ({
+  const [userInfo, setUserInfo] = useState({
     name: '',
     email: '',
     address: '',
     phone: '',
   });
 
+  const navigate = useNavigate();
+
+
   useEffect(() => {
     axios.get('/api/userInfo')
-    .then(res => {
-      setUserInfo(res.data);
-    })
-    .catch(err => {
-      console.error(err);
-    });
+      .then(res => {
+        setUserInfo(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }, []);
 
   const handleChange = (e) => {
     setUserInfo({
       ...userInfo,
-      [e.target.name]:e.target.value,
+      [e.target.name]: e.target.value,
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     axios.put('/api/userInfo', userInfo)
-    .then(res => {
-      alert('정보가 성공적으로 수정되었습니다');
-    })
-    .catch(err => {
-      console.error(err);
-    });
+      .then(res => {
+        alert('정보가 성공적으로 수정되었습니다');
+      })
+      .catch(err => {
+        console.error(err);
+      });
   };
 
   return (
-    <div className='myPage'>
+    <div className={styles.myPage}>
       <h1>마이페이지</h1>
       <form onSubmit={handleSubmit}>
         <label>
@@ -59,6 +63,7 @@ const MyPage = () => {
           <input type='tel' name='phone' value={userInfo.phone} onChange={handleChange} />
         </label>
         <button type='sumbmit'>정보 수정</button>
+          <button onClick={() => navigate("/main")}>취소</button>
       </form>
     </div>
   );
